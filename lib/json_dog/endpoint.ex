2dafedb3a086fce99d10624063656dd0783920b9
Luke Ledet <luke@lootbox.org>
2016-04-27 04:46:52 -0500

    Remove static files and channel

--- lib/json_dog/endpoint.ex
+++ lib/json_dog/endpoint.ex
@@ -1,8 +1,6 @@
 defmodule Dog.Endpoint do
   use Phoenix.Endpoint, otp_app: :json_dog
 
-  socket "/socket", Dog.UserSocket
-
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phoenix.digest
