ff27edaa7488c9981c7510ce71491248e2173cc3
Luke Ledet <luke@lootbox.org>
2016-04-28 03:27:43 -0500

    Provide some useful info

---
+++ web/controllers/json_controller.ex
@@ -0,0 +1,56 @@
+defmodule Dog.JSONController do
+  use Dog.Web, :controller
+
+  def index(conn, _) do
+    response = %{
+      endpoints: %{
+        get: [
+          "/about",
+          "/ip",
+          "/request_headers"
+        ],
+        post: [
+          "/request_headers"
+        ]
+      }
+    }
+
+    json conn, response
+  end
+
+  def about(conn, _) do
+    response = %{
+      created_by: "@lukeledet",
+      repo: "https://github.com/lukeledet/json_dog",
+      description: """
+      json.dog was created for debugging all kinds of web requests. It's a
+      free service that will remain free for.e.ver. If you think it's missing
+      any data, open an issue on the repo or, better yet, a pull request.
+      """
+    }
+
+    json conn, response
+  end
+
+  def ip(conn, _) do
+    response = %{
+      ip: conn.remote_ip |> Tuple.to_list |> Enum.join(".")
+    }
+
+    json conn, response
+  end
+
+  def request_headers(%{method: "GET"} = conn, _) do
+    response = Enum.into(conn.req_headers, %{})
+
+    json conn, response
+  end
+
+  def request_headers(%{method: "POST"} = conn, _) do
+    response = Enum.into(conn.req_headers, %{
+      body: conn.private[:raw_body]
+    })
+
+    json conn, response
+  end
+end
--- web/router.ex
+++ web/router.ex
@@ -5,7 +5,13 @@ defmodule Dog.Router do
     plug :accepts, ["json"]
   end
 
-  scope "/api", Dog do
+  scope "/", Dog do
     pipe_through :api
+
+    get "/", JSONController, :index
+    get "/ip", JSONController, :ip
+    get "/request_headers", JSONController, :request_headers
+    post "/request_headers", JSONController, :request_headers
+    get "/about", JSONController, :about
   end
 end
